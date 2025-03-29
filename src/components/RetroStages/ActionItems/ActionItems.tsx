ced22e6ea36ced365b7c7c7574dc249a13d3c27e
Fedor Kurbatov <kurbatov@gmail.com>
2025-03-29 07:17:12 +0000

    fixed PROD_URL not available when use client

--- src/components/RetroStages/ActionItems/ActionItems.tsx
+++ src/components/RetroStages/ActionItems/ActionItems.tsx
@@ -32,7 +32,7 @@ const ActionItems: React.FC<ActionItems> = ({ id, createdBy }) => {
       return acc;
     }, {} as { [key: string]: string });
 
-    fetch(new URL("/api/mailer", process.env.PROD_URL || "http://localhost:3000"), {
+    fetch(new URL("/api/mailer", process.env.NEXT_PUBLIC_PROD_URL || "http://localhost:3000"), {
       method: "POST",
       body: JSON.stringify({
         to: retro.everJoined,
--- src/socket.ts
+++ src/socket.ts
@@ -6,7 +6,7 @@ let socket: Socket | null = null;
 
 export const getSocket = (): Socket => {
   if (!socket) {
-    socket = io(process.env.PROD_URL || "http://localhost:3000");
+    socket = io(process.env.NEXT_PUBLIC_PROD_URL || "http://localhost:3000");
   }
   return socket;
 };
